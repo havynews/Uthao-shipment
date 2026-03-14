dd256387c0138a591c71ac18f9767444ce7d376b
chimenmagoodness <chimenmagoodness@gmail.com>
2026-03-14 22:22:10 +0100

    trying to fix the wsgi.py code

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,4 +1,5 @@
 # wsgi.py
+
 from gevent import monkey
 monkey.patch_all()
 
@@ -6,5 +7,12 @@ from app import create_app, socketio
 
 app = create_app()
 
-if __name__ == "__main__":
-    socketio.run(app)
+# from gevent import monkey
+# monkey.patch_all()
+
+# from app import create_app, socketio
+
+# app = create_app()
+
+# if __name__ == "__main__":
+#     socketio.run(app)
